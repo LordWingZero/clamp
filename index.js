ae2a533414d2231da69104b0c00edd3e9e7f2e33
Victorio Berra <toryberra@gmail.com>
2015-12-04 16:46:16 +0000

    Added tests

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = clamp;
 
+/**
+ * Returns a value that is between the provided min and max.
+ * If value is outside those, the min or max is returned respectively.
+ * @param {Number} value
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Number} clamped
+ */
 function clamp(value, min, max) {
   if (min > max) {
     return 0;
@@ -7,6 +15,11 @@ function clamp(value, min, max) {
   return Math.max(min, Math.min(max, value));
 }
 
-clamp.prototype = function saturate(v) {
-  return this.clamp(v, 0.0, 1.0);
+/**
+ * Shorthand for clamp, provies min as 0.0 and max as 1.0.
+ * @param {Number} value
+ * @return {Number} clamped
+ */
+clamp.prototype.saturate = function (v) {
+  return clamp(v, 0.0, 1.0);
 }
--- test.js
+++ test.js
@@ -8,4 +8,11 @@ test('clamp', function(t) {
   t.equal(clamp(100, 0, 50), 50)
   t.equal(clamp(50, 100, 150), 100)
   t.end()
+});
+
+test('saturate', function(t) {
+  t.equal(clamp.prototype.saturate(0), 0)
+  t.equal(clamp.prototype.saturate(0.05), 0.05)
+  t.equal(clamp.prototype.saturate(100), 1)
+  t.end()
 })
